6d4e5ec3642c123aaa233be9d150c4b4a2d90209
Frozen-molina <79784606+Frozen-molina@users.noreply.github.com>
2021-02-28 17:20:19 +0800

    messages

---
+++ messages/controller.php
@@ -0,0 +1,76 @@
+
+<?php
+require_once ("../../include/initialize.php");
+ 	 if (!isset($_SESSION['ADMIN_USERID'])){
+      redirect(web_root."admin/index.php");
+     }
+
+
+$action = (isset($_GET['action']) && $_GET['action'] != '') ? $_GET['action'] : '';
+
+switch ($action) {
+	case 'add' :
+	doInsert();
+	break;
+	
+	case 'edit' :
+	doEdit();
+	break;
+	
+	case 'delete' :
+	doDelete();
+	break;
+
+ 
+	}
+   
+	function doInsert(){
+		if(isset($_POST['save'])){
+
+
+		}
+	}
+
+	function doEdit(){
+		if(isset($_POST['submit'])){
+
+			$MSG = New Msg();
+			$MSG->MSGSTATS	= 'READ';
+			$MSG->update($_POST['msgid']);
+
+			message("[". $_POST['subject'] ."] has been marked as read!", "success");
+			redirect("index.php");
+		}
+
+	}
+
+
+	function doDelete(){
+		// if (isset($_POST['selector'])==''){
+		// message("Select a records first before you delete!","error");
+		// redirect('index.php');
+		// }else{
+
+			$id = $_GET['id'];
+
+			$category = New Category();
+			$category->delete($id);
+
+			message("Category already Deleted!","info");
+			redirect('index.php');
+
+		// $id = $_POST['selector'];
+		// $key = count($id);
+
+		// for($i=0;$i<$key;$i++){
+
+		// 	$category = New Category();
+		// 	$category->delete($id[$i]);
+
+		// 	message("Category already Deleted!","info");
+		// 	redirect('index.php');
+		// }
+		// }
+		
+	}
+?>
